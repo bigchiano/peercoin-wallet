9e94233b1377a6f4ffd38ea3e3d8b3568a41cad7
bigchiano <bigchiano@protonmail.com>
2022-09-23 23:01:35 +0300

    added about and settings page'

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import Transaction from "./pages/transaction";
 import Verify from "./pages/verify";
 import Sign from "./pages/sign";
 import BroadCast from "./pages/broadcast";
+import Settings from "./pages/settings";
+import About from "./pages/about";
 
 function App() {
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/about" element={<About />} />
           <Route path="/broadcast" element={<BroadCast />} />
           <Route path="/sign" element={<Sign />} />
           <Route path="/verify" element={<Verify />} />
