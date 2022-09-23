29373931d87a3a1e6af4606e734a0d348ed777d9
bigchiano <bigchiano@protonmail.com>
2022-09-23 22:21:25 +0300

    added transaction page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Address from "./pages/address";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout";
 import Home from "./pages/index";
+import Transaction from "./pages/transaction";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/address/*" element={<Address />} />
+          <Route path="/transaction/*" element={<Transaction />} />
           <Route path="/*" element={<Home />} />
         </Routes>
       </Layout>
